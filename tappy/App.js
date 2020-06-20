b6d2f38cc3ab74c5cc5a7f671fd23a3171efac08
Tyler Htet Naing Phyo <h.n.p.tyler.shwe@gmail.com>
2020-06-20 21:47:45 +0900

    last updata on screen format

--- tappy/App.js
+++ tappy/App.js
@@ -1,11 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import {
   View,
